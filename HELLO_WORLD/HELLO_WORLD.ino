d15ee222f6413e167b664e1d01f0fa90ce1f5d63
Cro1995 <rubiocarlos327@gmail.com>
2017-01-30 20:25:53 +0100

    HELLO WORLD

---
+++ HELLO_WORLD/HELLO_WORLD.ino
@@ -0,0 +1,32 @@
+/****************************************************************
+ **                                                            **
+ **                    HELLO WORLD                             **                                                           
+ **                    SERIAL                                  **                   
+ **                                                            **
+ **  Nom: Carlos Rubio Ortega               Data: 30/01/2017   **                                                           
+ ***************************************************************/
+ //************************ INCLUDE *****************************
+
+
+ //*********************** VARIABLES ****************************
+
+
+
+
+ //************************* SETUP ******************************
+
+void setup()                // configura el final de la sortida 
+{
+  Serial.begin(9600);       // configurar una bibloteca al serial a 9600 bps
+  Serial.println("Hello world!"); // ens diu Hola mundo i tanca la linea, la pròxima linia començaria a sota. 
+}
+//*************************** LOOP ******************************
+
+void loop() {          // inicia el bucle del programa 
+  
+                       //  no fa cap bucle 
+}
+
+
+//************************* FUNCIONS ****************************
+
