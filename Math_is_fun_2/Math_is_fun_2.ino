a6a67885d4642c97e5490ef6d3a2bfe28db2f883
Cro1995 <rubiocarlos327@gmail.com>
2017-02-06 20:00:50 +0100

    Hipotenusa

--- Math_is_fun_2/Math_is_fun_2.ino
+++ Math_is_fun_2/Math_is_fun_2.ino
@@ -13,7 +13,9 @@
 int a = 3;        // definim les variables quin valor seran 
 int b = 4;       // definim les variables quin valor seran 
 int h;          // definim les variables que en aquest cas no sabem el seu valor  
-
+int resultat1; // Guarda el resultat de la variable a
+int resultat2; // Guarda el resultat de la variable b
+int resultat3; // Guarda el resultat de la variable h
 
 
  //************************* SETUP ******************************
@@ -22,33 +24,34 @@ void setup()                // configura el final de la sortida
 {
   Serial.begin(9600);       // configurar una bibloteca al serial a 9600 bps
 
-  Serial.println("Aqui tenim unes quantes operacions"); // ens diu Aqui tenim unes quantes operacions i tanca la linea, la pròxima linia començaria a sota. 
+  Serial.println("Aqui tenim un calcul de una hipotenusa"); // ens diu Aqui tenim un calcul de una hiputenusa i tanca la linea, la pròxima linia començaria a sota. 
   Serial.print("a = ");                 // Llegeix el valor de la variable 
   Serial.println(a);                    // Ens diu el valor de la variable 
   Serial.print("b = ");                 // Llegeix el valor de la variable
   Serial.println(b);                    // Ens diu el valor de la variable
-  Serial.print("h = ");                 // Llegeix el valor de la variable
+  Serial.print("h = ?");                 // Llegeix el valor de la variable
   Serial.println(h);                    // Ens diu el valor de la variable
 
-  Serial.print(double pow(a)+(double pow(b));       // Llegeix el valor de les variables i fa la suma 
-  Serial.println(a + b);          // ens diu el resultat de la suma 
-  delay(1000);                    // s'espera 1 segon 
-  
-  Serial.print("a * b = ");       // Llegeix el valor de les variables i fa la multiplicació 
-  Serial.println(a * b);          // Ens diu el resultat de la multiplicació 
-  delay(2000);                    // S'espera 2 segons 
-  
-  Serial.print("a / b = ");       // Llegeix el valor de les variables i fa la divisió 
-  Serial.println(a / b);          // Ens diu el resultat de la divisió sense decimals
+  Serial.println("Ens diu el valor del quadrat de la variable a");   // Posa en el arduino "Ens diu el valor del quadrat de la variable a"
+  Serial.print(resultat1=pow(3,2)); // Llegeix el valor de les variables i fa el quadrat de la variable a 
+  Serial.println();              // ens diu el resultat del quadrat 
+  delay(1000);                     // s'espera 1 segon 
+
+  Serial.println("Ens diu el valor del quadrat de la variable b"); // Posa en el arduino "Ens diu el valor del quadrat de la variable b"
+  Serial.print(resultat2=pow (4,2));       // Llegeix el valor de les variables i fa el quadrat de la variable b 
+  Serial.println();                      // Ens diu el resultat del quadrat  
+  delay(2000);                            // S'espera 2 segons 
+
+  Serial.println("Ens diu el valor de la suma de les variables a+b"); // Posa en el arduino "Ens diu el valor de la suma de les variables a+b"
+  Serial.print(resultat3= resultat1+resultat2);       // Llegeix el valor de les variables i fa la suma
+  Serial.println();          // Ens diu el resultat de la suma
   delay(3000);                    // S'espera 3 segons 
+
+  Serial.println("Ens diu el valor de l'arrel quadrada"); // Posa en el arduino "Ens deiu el valor de l'arrel quadrada"
+  Serial.println(sqrt(resultat3));          // Ens diu el resultat de l'arrel quadrada 
+  delay(4000);                              // S'espera 4 segons 
   
-  Serial.print("a % b = ");       // Llegeix el valor de les variables i fa la divisió 
-  Serial.println(a % b);          // Ens diu el resultat de la divisió amb el residu 
-  delay(4000);                    // S'espera 4 segons 
-  
-  Serial.print("b - a = ");       // Llegeix el valor de les variables i fa la resta 
-  Serial.println(b - a);          // Ens diu el resultat de la resta 
-  delay(1500);                    // S'espera 1.5 segons 
+ 
   
 }
 
@@ -63,5 +66,5 @@ void loop() {          // inicia el bucle del programa
 
 
 //************************* FUNCIONS ****************************
-   // En aquest programa podem veure com fa unes operacions.  
+   // En aquest programa podem veure com fa el calcul de la hipotenusa.  
    
